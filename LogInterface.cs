2ff56ff5ba675911eec32ea9ca0942001b3504a2
VoiceOf90s <60218217+VoiceOf90s@users.noreply.github.com>
2020-01-30 14:09:36 +0300

    core

---
+++ LogInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    interface LogInterface
+    {
+        //void log(string str); 
+        //void write(); 
+    }
+}
